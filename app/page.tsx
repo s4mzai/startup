e2c0892b73e4791569e9340a46717c48dbaf9194
samar <hi.samar.7x@gmail.com>
2025-07-05 18:26:57 +0530

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       {/* STARTUPS SECTION HERE */}
       <StartupSection/>
       {/* FOOTER HERE */}
-      <FooterSection/>  
+      <FooterSection/>
     </>
   );
 }
